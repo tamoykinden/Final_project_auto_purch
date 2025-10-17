c519df137a712365c50a8d12de158f8a6766bbb8
Denis Tamoykin <denistamoykin04@gmail.com>
2025-10-17 14:18:41 +0300

    Change secret_key

--- final_work_auto_purch/settings.py
+++ final_work_auto_purch/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 from os import getenv
 
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-avek4k5id*&-!i6i0ggjq6(a@kbh^h6#1#sm%8zb%ft2qrl)7d'
+SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
